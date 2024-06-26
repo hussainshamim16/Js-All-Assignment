40be091c02cb3ce5e795343c20ed5411268badc9
hussainshamim16 <hj422748@gmail.com>
2024-06-26 13:20:58 +0500

    second assignment Done

---
+++ Assignment 02/script.js
@@ -0,0 +1,34 @@
+// variables for strings
+
+console.log("hello world")
+
+var userName;
+var MyName = "M. Husain Shamim"
+
+var jsmessage = "Hy Js"
+var Hello  = "Hello world"
+// alert(Hello)
+
+var StudentName = "ali rehman"
+var studentAge = 16;
+var studenCertifecate = "WMA"
+
+// alert(StudentName)
+// alert(studentAge)
+// alert(studenCertifecate)
+
+var pizza = "pizza \n pizz \n piz \n pi \n p"
+// alert(pizza)
+
+var Email = "my email is hj422748@gmail.com"
+alert(Email)
+
+var book = "my Js book \n Easy To Learn Js"
+alert(book)
+
+var mart = "Hy devloper family !"
+document.write(mart)
+
+var rat = "(^///^)☆*: .｡. o(≧▽≦)o .｡.:*☆"
+alert(rat)
+
