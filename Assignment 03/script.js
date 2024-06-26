17faceccf8ce1144f018585b73f0955e4628534a
hussainshamim16 <hj422748@gmail.com>
2024-06-26 13:55:58 +0500

    done assignment 03

---
+++ Assignment 03/script.js
@@ -0,0 +1,16 @@
+// numbers 
+
+var age = 17;
+alert("i am"+age+" years old")
+
+
+alert("You have visited this site " +24 + " times")
+
+var myBirthday = "My birth year is";
+var date = 2007;
+var collect = myBirthday + " " +date;
+alert(collect)
+
+var tNumber = 1
+var visiter = "“John Doe ordered "+ tNumber +" T-shirt(s) on XYZ Clothing store”"
+alert(visiter);
